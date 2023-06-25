9d29b930169e85004809c3d0251502afac0020bb
Alex Weber <atweber@purdue.edu>
2023-06-24 21:38:12 -0500

    added start game command

--- gameEngine.py
+++ gameEngine.py
@@ -8,5 +8,7 @@ functions
 async def gameStream(message, cmd):
     print("gameEngine recieved this command: " + cmd)
     
-    if cmd == "load":
-        await msgChan(message, "Starting Secret Hitler... \n type !join to join and !start to start")
+    if cmd.split(":")[0] == "load":
+        await msgChan(message, "Starting Secret Hitler... \ntype !join to join and !start to start")
+    elif cmd.split(":")[0] == "join":
+        await msgDm(message, "Hello Welcome to Secret Hitler! \nPlease provide a Nickname...")
